031c56990d6d6bfd5aa544af9d868937026c1288
ZiadIsmaeil <esmail.saba@student.guc.edu.eg>
2025-05-02 23:10:49 +0300

    first commit

---
+++ NotificationService/src/main/java/com/example/piazza/notificationservice/model/Notification.java
@@ -0,0 +1,92 @@
+package com.example.piazza.notificationservice.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String type;
+    private String message;
+    private boolean read;
+    private boolean archived;
+    private LocalDateTime timestamp;
+
+    public Notification() {
+        this.timestamp = LocalDateTime.now();
+        this.read = false;
+        this.archived = false;
+    }
+
+    public Notification(String message, Long id, String userId, String type,  boolean archived, LocalDateTime timestamp) {
+        this.message = message;
+        this.id = id;
+        this.userId = userId;
+        this.type = type;
+        this.archived = archived;
+        this.timestamp = timestamp;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+// Getters and Setters omitted for brevity
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    // You can generate them with IntelliJ (Alt + Insert or Right click > Generate)
+}
---
+++ NotificationService/src/test/java/com/example/piazza/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piazza.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
