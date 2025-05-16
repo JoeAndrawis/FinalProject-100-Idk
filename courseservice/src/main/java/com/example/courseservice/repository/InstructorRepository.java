1159f5323a0d98209a510b9f20d03fbba6e74e25
JoeAndrawis <youssefsamy2031@gmail.com>
2025-05-16 15:35:18 +0300

    Delete courseservice directory

--- courseservice/src/main/java/com/example/courseservice/repository/InstructorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseservice.repository;
-
-import com.example.courseservice.model.Instructor;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InstructorRepository extends MongoRepository<Instructor, String> {
-}
