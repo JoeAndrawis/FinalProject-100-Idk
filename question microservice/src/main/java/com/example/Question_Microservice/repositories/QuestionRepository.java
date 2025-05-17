481b72b0f58adccee7f0f83052359bca022c5a72
Moshathecatto <163202803+Moshathecatto@users.noreply.github.com>
2025-05-17 17:26:27 +0300

    Delete question microservice directory

--- question microservice/src/main/java/com/example/Question_Microservice/repositories/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Question_Microservice.repositories;
-
-import com.example.Question_Microservice.models.Question;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends MongoRepository<Question, String> {
-}
