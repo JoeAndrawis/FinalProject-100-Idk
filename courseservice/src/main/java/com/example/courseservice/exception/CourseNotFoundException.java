7ba58daa50f6af32af4f7e18e7c41546e5038bd4
EsmailEssamSaba <esmail.saba@student.guc.edu.eg>
2025-05-19 01:36:36 +0300

    Revert "Message queue"

--- courseservice/src/main/java/com/example/courseservice/exception/CourseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseservice.exception;
-
-public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String id) {
-        super("Course not found with id: " + id);
-    }
-}
-
-
